768aa9d2669751231fbbef3da4bc713a60591ef0
BrandonStambuk <115127679+BrandonStambuk@users.noreply.github.com>
2024-04-22 12:45:03 -0400

    cambios en el frontend de homepage

--- frontend_entornos/src/components/HomePage.js
+++ frontend_entornos/src/components/HomePage.js
@@ -5,8 +5,13 @@ const Homepage = () => {
   return (
     <div className="homepage-container">
       <div className="content-wrapper">
-        <h1 className="title">Rapid Coder - Entornos Virtuales UMSS</h1>
-        <a href="/ejercicio" className="start-button">Start</a>
+        <div className="container-bienvenida">
+          <h1 className="title">Rapid Coder - Entornos Virtuales UMSS</h1>
+          <p className="description">Bienvenido a Rapid Coder, donde puedes mejorar tus habilidades en programación.
+          </p>
+          
+          <a href="/ejercicio" className="start-button">Start</a>
+        </div>
       </div>
     </div>
   );
