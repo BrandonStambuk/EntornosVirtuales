fa1106be438261f8a146761c150b144fe3707404
Arieleandrovv <Arieleandrovv@outlook.com>
2024-05-27 21:23:08 -0400

    login correcto

--- frontend_entornos/src/App.js
+++ frontend_entornos/src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-//import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 import MostrarEjercicios from './components/MostrarEjercicios';
 import AgregarEjercicio from './components/AgregarEjercicio';
 import Homepage from './components/HomePage';
 import HacerEjercicios from './components/HacerEjercicio';
-import Navbar from './components/Navbar';
 import Ejercicio from './components/Ejercicio';
 import RegistrarAlumno from './components/RegistroAlumno';
 import InicioSesion from './components/InicioSesion';
 import AlumnoStats from './components/AlumnoStats';
 import HacerEjercicioNoBackSpace from './components/HacerEjercicioNoBackSpace';
-//import'./css/App.css';
+
 const App = () => {
-  const userData = JSON.parse(localStorage.getItem('is_profesor'));
+  const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('userData')));
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setUserData(JSON.parse(localStorage.getItem('userData')));
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    setUserData(JSON.parse(localStorage.getItem('userData')));
+  }, []);
 
   const ProfesorProtectedRoute = ({ children }) => {
-    if (userData) {
+    if (userData.is_profesor) {
       return children;
     } else {
       return <Navigate to="/" />;
@@ -25,7 +40,7 @@ const App = () => {
   };
 
   const StudentProtectedRoute = ({ children }) => {
-    if (!userData) {
+    if (!userData.is_profesor) {
       return children;
     } else {
       return <Navigate to="/" />;
@@ -35,20 +50,20 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-       <Navbar></Navbar>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/agregar' element={<ProfesorProtectedRoute><AgregarEjercicio /></ProfesorProtectedRoute>} />
           <Route path='/mostrar' element={<StudentProtectedRoute><MostrarEjercicios /></StudentProtectedRoute>} />
           <Route path='/mostrar/:id' element={<StudentProtectedRoute><HacerEjercicios /></StudentProtectedRoute>} />
           <Route path='/mostrar-NoBackspace/:id' element={<StudentProtectedRoute><HacerEjercicioNoBackSpace /></StudentProtectedRoute>} />
-          <Route path='/registrar-alumno'element={<ProfesorProtectedRoute><RegistrarAlumno/></ProfesorProtectedRoute>} />
-          <Route path='/inicio-sesion' element={<InicioSesion/>}/>
-          <Route path='/stats' element={<AlumnoStats/>}/>
-        </Routes>      
+          <Route path='/registrar-alumno' element={<ProfesorProtectedRoute><RegistrarAlumno /></ProfesorProtectedRoute>} />
+          <Route path='/inicio-sesion' element={<InicioSesion />} />
+          <Route path='/stats' element={<AlumnoStats />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- frontend_entornos/src/components/InicioSesion.js
+++ frontend_entornos/src/components/InicioSesion.js
@@ -1,91 +1,98 @@
 import React, { useState } from "react";
-import axios from 'axios'
-import { URL_API } from "./const";
-import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const endpoint = URL_API;
+const endpoint = "http://localhost:8000/api"; // Asegúrate de actualizar la URL base correcta.
 
-const InicioSesion = () => {  
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+const InicioSesion = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleEmailChange = (email) => {
-        setEmail(email.target.value);
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFindAlumno = async (e) => {
+    e.preventDefault();
+    let is_alumno = true;
+
+    try {
+      const response = await axios.post(`${endpoint}/profesor/find`, {
+        email: email,
+        password: password
+      });
+
+      if (response.data) {
+        localStorage.removeItem("userData");
+        localStorage.setItem("userData", JSON.stringify({ id: response.data.id, is_profesor: true }));
+        is_alumno = false;
+        console.log("Respuesta del servidor:", response.data);
+
+        
+          navigate("/registrar-alumno");
+          window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error al buscar profesor:", error);
     }
-    const handlePasswordChange = (password) => {
-        setPassword(password.target.value);
-    }
 
-    const handleFindAlumno = async (e) => {
-        e.preventDefault();
-        let is_alumno = true;
-        try {
-            const response = await axios.post(`${endpoint}/profesor/find`, {
-                email: email,
-                password: password
-            });
-            if (response.data){
-                localStorage.setItem("profesorData", JSON.stringify(response.data),{is_profesor: true});
-                is_alumno=false;
-                console.log("Respuesta del servidor:", response.data, {is_profesor: true});
-                navigate("/registrar-alumno");
-            }
-            
-        } catch (error) {
-            console.error("Error al buscar profesor:", error);
+    if (is_alumno) {
+      try {
+        const responseAlumno = await axios.post(`${endpoint}/alumnos/find`, {
+          email: email,
+          password: password
+        });
+
+        if (responseAlumno.data) {
+          localStorage.removeItem("userData");
+          localStorage.setItem("userData", JSON.stringify({ id: responseAlumno.data.id, is_profesor: false }));
+          console.log("Respuesta del servidor:", responseAlumno.data);
+
+          
+            navigate("/mostrar");
+            window.location.reload();
         }
-        if (is_alumno){
-            try {
-                const responseAlumno = await axios.post(`${endpoint}/alumnos/find`, {
-                    email: email,
-                    password: password
-                });
-                if (responseAlumno.data){
-                    localStorage.setItem("alumnoData", JSON.stringify(responseAlumno.data),{is_profesor: false});
-                    console.log("Respuesta del servidor:", responseAlumno.data, {is_profesor: false});
-                    navigate("/mostrar");
-                }
-                
-            } catch (error) {
-                console.error("Error al buscar alumno:", error);
-            }            
-        }
-
-                
+      } catch (error) {
+        console.error("Error al buscar alumno:", error);
+      }
     }
+  };
 
-    return (
-        <div className="container d-flex justify-content-center align-items-center vh-100">
-            <div className="card" >
-                <div className="card-body">
-                    <h1 className="card-title text-center">Inicio Sesion</h1>
-                    <form>
-                        <div className='mb-3'>
-                            <label className='form-label'>Email</label>
-                            <input 
-                                value={email} 
-                                onChange={ handleEmailChange}
-                                type='email'
-                                className='form-control shadow-sm'
-                            />
-                        </div>
-                        <div className='mb-3'>
-                            <label className='form-label'>Password</label>
-                            <input 
-                                value={password} 
-                                onChange={ handlePasswordChange}
-                                type='password'
-                                className='form-control shadow-sm'
-                            />
-                        </div>              
-                        <button onClick={handleFindAlumno}
-                         className='btn btn-success'
-                         >Inicio Sesion</button>
-                    </form>
-                </div>
+  return (
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card">
+        <div className="card-body">
+          <h1 className="card-title text-center">Inicio Sesión</h1>
+          <form>
+            <div className='mb-3'>
+              <label className='form-label'>Email</label>
+              <input
+                value={email}
+                onChange={handleEmailChange}
+                type='email'
+                className='form-control shadow-sm'
+              />
             </div>
+            <div className='mb-3'>
+              <label className='form-label'>Password</label>
+              <input
+                value={password}
+                onChange={handlePasswordChange}
+                type='password'
+                className='form-control shadow-sm'
+              />
+            </div>
+            <button onClick={handleFindAlumno} className='btn btn-success'>Inicio Sesión</button>
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+
 export default InicioSesion;
